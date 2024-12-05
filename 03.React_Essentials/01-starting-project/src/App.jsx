eca0960967fb1df8327d1484225951e4696757eb
na-kyoung <skrud310@naver.com>
2024-12-05 17:30:06 +0900

    Day 04

--- 03.React_Essentials/01-starting-project/src/App.jsx
+++ 03.React_Essentials/01-starting-project/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -20,12 +22,55 @@ function Header(){
   );
 }
 
+function CoreConcept(props){
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function CoreConcept_dist({ title, description, image }){
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function CoreConcept_group({ concept }){
+  const { title, description, image } = concept;
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header/>
       <main>
+        <section id="core-concepts">
         <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcept 
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcept {...CORE_CONCEPTS[1]} />
+          <CoreConcept_dist {...CORE_CONCEPTS[2]} />
+          <CoreConcept_group concept = {CORE_CONCEPTS[3]} />
+        </ul>
+        </section>
       </main>
     </div>
   );
