a4273451a2ea08dabcc56650d014b295f41fba8d
na-kyoung <skrud310@naver.com>
2025-01-07 17:03:30 +0900

    Day 19. dialog (useImperativeHandle & createPortal)

--- 08.Refs_Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ 08.Refs_Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -4,14 +4,8 @@ import ResultModal from "./ResultModal";
 export default function TimerChallenge({ title, targetTime }){
     const timer = useRef(); // 타이머
     const dialog = useRef(); // 모달창
-    // const [timerStarted, setTimerStarted] = useState(false); // 타이머 시작
-    // const [timerExpired, setTimerExpired] = useState(false); // 타이머 종료
     const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000); // 시간 측정
     const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000; // 타이머 활성화
-
-    console.log("targetTime : ", targetTime);
-    console.log("timeRemaining : ", timeRemaining);
-    console.log("timerIsActive : ", timerIsActive);
 
     // 타이머 만료
     if(timeRemaining <= 0){
@@ -26,24 +20,14 @@ export default function TimerChallenge({ title, targetTime }){
 
     // 타이머 시작
     function handleStart(){
-        console.log(timeRemaining);
-        // timer.current = setTimeout(() => {
-        //     setTimerExpired(true);
-        //     dialog.current.open();
-        // }, targetTime * 1000);
-
         // 10밀리초마다 실행됨
         timer.current = setInterval(() => {
             setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10); // 10밀리초씩 빼기
-            console.log(timeRemaining);
         }, 10);
-
-        // setTimerStarted(true);
     }
 
     // 타이머 종료
     function handleStop(){
-        // clearTimeout(timer.current); // 타이머 종료
         dialog.current.open();
         clearInterval(timer.current);
     }
