4f93bccc0da48ca74dcbd2c24531ab4dacab94e6
na-kyoung <skrud310@naver.com>
2025-01-10 14:31:54 +0900

    Day 23. useEffect’s Cleanup & Function Dependencies

--- 11.SideEffect_useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 11.SideEffect_useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  console.log('DeleteConfirmation Start');
+
+  useEffect(() => {
+    // 3초 뒤 자동 삭제
+    console.log('Timer Set!');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    // 컴포넌트가 사라지기 직전에 실행
+    return () => {
+      console.log('Cleaning Up Timer')
+      clearTimeout(timer);
+    }
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
