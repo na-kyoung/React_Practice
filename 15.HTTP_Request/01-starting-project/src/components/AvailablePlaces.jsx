5d04a2ac4cf1a9c8cdbbbdb9a82936dde7dfd1d6
na-kyoung <skrud310@naver.com>
2025-01-14 17:27:12 +0900

    Day 27. HTTP Error

--- 15.HTTP_Request/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15.HTTP_Request/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,15 +1,37 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   // fetching places data (async&await)
   useEffect(() => {
     async function fetchPlaces() {
-      const response = await fetch('http://localhost:3000/places'); 
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try{
+        const response = await fetch('http://localhost:3000/places'); 
+        const resData = await response.json();
+
+        if(!response.ok){
+          throw new Error('Failed to fetch places');
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+      } catch (err) {
+        // setError(err);
+        setError({message: err.message || 'Could not fetch places, please try again later.'});
+        setIsFetching(false);
+      }
     }
 
     fetchPlaces();
@@ -26,10 +48,18 @@ export default function AvailablePlaces({ onSelectPlace }) {
   //   });
   // }, []);
 
+
+  // 오류 발생 시 메세지 출력
+  if (error) {
+    return <ErrorPage title="An error occurred!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
