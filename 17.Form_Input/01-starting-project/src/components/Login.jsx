dfaa118a0354c8e936a6734551aabb264763ac48
na-kyoung <skrud310@naver.com>
2025-01-16 10:47:11 +0900

    Revert "Day 30. Input & Submission with useRef"
    This reverts commit 284451c4ccf259e6a6709efb741670a14c394ebf.

--- 17.Form_Input/01-starting-project/src/components/Login.jsx
+++ 17.Form_Input/01-starting-project/src/components/Login.jsx
@@ -1,17 +1,35 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export default function Login() {
-  const email = useRef();
-  const password = useRef();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enterdPassword, setEnterdPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
 
   function handleSubmit(event){
     event.preventDefault(); // 이벤트 발생 막음
-
-    const enteredEMail = email.current.value;
-    const enteredPassword = password.current.value;
-
-    console.log(enteredEMail, enteredPassword);
+    console.log(enteredValues);
+    // console.log('User Email : ' + enteredValues.email);
+    // console.log('User Password : ' + enteredValues.password);
   }
+
+  function handleInputChange(identifier, value){
+    // 입력창에 보이게 설정
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: value
+    }));
+  }
+
+  // function handleEmailChange(event){
+  //   setEnteredEmail(event.target.value); // 입력창에 보이게 설정
+  // }
+
+  // function handlePasswordChange(event){
+  //   setEnterdPassword(event.target.value);
+  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -21,15 +39,15 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email"
-            ref={email}
-          />
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input id="password" type="password" name="password"
-          ref={password}
-          />
+            onChange={(event) => handleInputChange('password', event.target.value)}
+            value={enteredValues.password}/>
         </div>
       </div>
 
