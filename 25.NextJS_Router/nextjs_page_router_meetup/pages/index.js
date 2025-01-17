7fe5346657ff3b61bcff7f4cf8439a7be0436c2c
na-kyoung <skrud310@naver.com>
2025-01-17 17:55:34 +0900

    Day 37. Output List (React Component)

--- 25.NextJS_Router/nextjs_page_router_meetup/pages/index.js
+++ 25.NextJS_Router/nextjs_page_router_meetup/pages/index.js
@@ -1,2 +1,30 @@
 // our-domain.com/
+import MeetupList from "@/components/meetups/MeetupList";
 
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Eiffel_Tower_from_the_Tour_Montparnasse%2C_1_May_2012_N2.jpg/640px-Eiffel_Tower_from_the_Tour_Montparnasse%2C_1_May_2012_N2.jpg',
+        address: 'Some address 5, some City',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Eiffel_Tower_from_the_Tour_Montparnasse%2C_1_May_2012_N2.jpg/640px-Eiffel_Tower_from_the_Tour_Montparnasse%2C_1_May_2012_N2.jpg',
+        address: 'Some address 10, some City',
+        description: 'This is a second meetup!'
+    }
+];
+
+function HomePage(){
+
+    return (
+        <>
+            <MeetupList meetups={DUMMY_MEETUPS}/>
+        </>
+    )
+}
+
+export default HomePage;
