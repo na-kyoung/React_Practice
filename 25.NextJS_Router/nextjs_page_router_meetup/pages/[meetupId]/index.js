1d94f59e514135c85ea207c5200876d71fe7e6b9
na-kyoung <skrud310@naver.com>
2025-01-20 11:11:28 +0900

    Day 38. Programmatic Navigation &  CSS Module

--- 25.NextJS_Router/nextjs_page_router_meetup/pages/[meetupId]/index.js
+++ 25.NextJS_Router/nextjs_page_router_meetup/pages/[meetupId]/index.js
@@ -1,2 +1,15 @@
 // our-domain.com/[meetupId]
 
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+    return (
+        <MeetupDetail image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Eiffel_Tower_from_the_Tour_Montparnasse%2C_1_May_2012_N2.jpg/640px-Eiffel_Tower_from_the_Tour_Montparnasse%2C_1_May_2012_N2.jpg"
+            title="A First Meetup"
+            address="Some Street 5, Som City"
+            description="The meetup description"
+        />
+    );
+}
+
+export default MeetupDetails;
--- 25.NextJS_Router/nextjs_page_router_meetup/pages/new-meetup/index.js
+++ 25.NextJS_Router/nextjs_page_router_meetup/pages/new-meetup/index.js
@@ -1,2 +1,13 @@
 // our-domain.com/new-meetup
 
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
+function NewMeetupPage(){
+    function addMeetupHandler(eneteredMeetupData){
+        console.log(eneteredMeetupData);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
