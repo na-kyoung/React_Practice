ad58dd5a5a8b91618a86e72bc4cc32153bee0668
na-kyoung <skrud310@naver.com>
2025-03-13 14:41:40 +0900

    Day 48. Static/Dynamic Metadata

--- 25.NextJS_Router/05-foodies-project/app/meals/[mealSlug]/page.js
+++ 25.NextJS_Router/05-foodies-project/app/meals/[mealSlug]/page.js
@@ -1,5 +1,20 @@
 // http://localhost:3000/meals/[mealSlug]
 
+import { notFound } from "next/navigation";
+
+export async function generateMetadata(params) {
+  const meal = getMeal(params.mealSlug);
+
+  if(!meal){
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage(){
   return (
     <>
